03f83b920b856935d8699a96b392e1d187bddc22
RAZER\Nikita <gurskynikita@gmail.com>
2021-01-31 01:10:52 +0300

    Fixed burg overview tab

--- modules/ui/burgs-overview.js
+++ modules/ui/burgs-overview.js
@@ -66,23 +66,23 @@ function overviewBurgs() {
 
     for (const b of filtered) {
       const population = b.population * populationRate.value * urbanization.value;
-      const prosperity = b.prosperity;
       totalPopulation += population;
       const type = b.capital && b.port ? "a-capital-port" : b.capital ? "c-capital" : b.port ? "p-port" : "z-burg";
       const state = pack.states[b.state].name;
       const prov = pack.cells.province[b.cell];
       const province = prov ? pack.provinces[prov].name : "";
       const culture = pack.cultures[b.culture].name;
+      const prosperity = b.prosperity;
 
       lines += `<div class="states" data-id=${b.i} data-name="${b.name}" data-state="${state}" data-prosperity="${prosperity}" data-province="${province}" data-culture="${culture}" data-population=${population} data-type="${type}">
         <span data-tip="Click to zoom into view" class="icon-dot-circled pointer"></span>
         <input data-tip="Burg name. Click and type to change" class="burgName" value="${b.name}" autocorrect="off" spellcheck="false">
         <input data-tip="Burg province" class="burgState" value="${province}" disabled>
         <input data-tip="Burg state" class="burgState" value="${state}" disabled>
-        <input data-tip="Burg prosperity. Type to change" class="burgProsperity" value=${prosperity}>
         <select data-tip="Dominant culture. Click to change burg culture (to change cell cultrure use Cultures Editor)" class="stateCulture">${getCultureOptions(b.culture)}</select>
         <span data-tip="Burg population" class="icon-male"></span>
         <input data-tip="Burg population. Type to change" class="burgPopulation" value=${si(population)}>  
+        <input data-tip="Burg prosperity. Type to change" class="burgProsperity" value=${prosperity}>
         <div class="burgType">
           <span data-tip="${b.capital ? ' This burg is a state capital' : 'Click to assign a capital status'}" class="icon-star-empty${b.capital ? '' : ' inactive pointer'}"></span>
           <span data-tip="Click to toggle port status" class="icon-anchor pointer${b.port ? '' : ' inactive'}" style="font-size:.9em"></span>
